9ed9a9a896678d47776186c01bbb58b438550885
tlsrb100 <tlsrb100@gmail.com>
2022-11-11 18:37:03 +0900

    콘솔로그 추가

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import { C } from './components/C';
 import { Upper } from './components/Upper';
 
 function App() {
-     const testFunc = () => {
-          console.log('dasd');
-     };
-     return (
-          <BrowserRouter>
-               <Routes>
-                    <Route element={<A />}>
-                         <Route path="/b" element={<Upper />} />
-                    </Route>
-                    <Route element={<C />}>
-                         <Route path="/c" element={<B />} />
-                    </Route>
-               </Routes>
-          </BrowserRouter>
-     );
+  const testFunc = () => {
+    console.log('dasd');
+    console.log('dasd');
+    console.log('dasd');
+    console.log('dasd');
+  };
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<A />}>
+          <Route path="/b" element={<Upper />} />
+        </Route>
+        <Route element={<C />}>
+          <Route path="/c" element={<B />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
